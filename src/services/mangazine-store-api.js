af142fd4e6b47b097cac71d5dc3400fa09039bf4
Kevin Alves <kevin.alves.dev@gmail.com>
2023-01-27 23:09:17 -0300

    add auth to api requests and remove file

--- src/services/mangazine-store-api.js
+++ src/services/mangazine-store-api.js
@@ -4,14 +4,14 @@ const apiRequests = axios.create({
   baseURL: process.env.REACT_APP_API_URL
 });
 
+const authorization = (token) => ({
+  headers: { 'Authorization': `Bearer ${token}` }
+});
+
 export const signIn = ({ email, password }) => (
   apiRequests.post("/auth/sign-in", { email, password })
 );
 
 export const signUp = ({ name, email, password }) => (
   apiRequests.post("/users", { name, email, password, address })
-);
-
-export const signOut = () => (
-  apiRequests.delete("/auth/sign-out")
 );
