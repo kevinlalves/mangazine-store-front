b0018052e5244f89a05ce3f7e463bcf1a8139c72
Kevin Alves <kevin.alves.dev@gmail.com>
2023-01-28 13:30:35 -0300

    fix breaking changes in main

--- src/components/loadingProducts/LoadingProducts.js
+++ src/components/loadingProducts/LoadingProducts.js
@@ -1,18 +1,20 @@
 import { Rings } from 'react-loader-spinner'
 import StyledLoadingContainer from './LoadingProducts.styled'
-const LoadingProducts = () => {
-    return (
-        <StyledLoadingContainer>
-        <Rings
-  height="200"
-  width="200"
-  radius="9"
-  color="#243763"
-  ariaLabel="loading"
-  wrapperStyle
-  wrapperClass
-/></StyledLoadingContainer>
-    )
-}
 
-export default LoadingProducts
+const LoadingProducts = () => {
+  return (
+    <StyledLoadingContainer>
+      <Rings
+        height="200"
+        width="200"
+        radius="9"
+        color="#243763"
+        ariaLabel="loading"
+        wrapperStyle
+        wrapperClass
+      />
+    </StyledLoadingContainer>
+  );
+};
+
+export default LoadingProducts;
